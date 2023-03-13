b8d68cbe94fe2e4448cb9f1c28d0ea4dfcb5e6e7
Louise BERTIN <louise@trustfolio.co>
2023-03-13 16:21:01 +0100

    Working router-vue and tailwind css

--- calories-calculator/src/router/index.js
+++ calories-calculator/src/router/index.js
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import MainLayout from '../layout/MainLayout.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: Home,
-    },
-    {
-        path: '/recipies',
-        name: 'Recipies',
-        component: Home,
+        component: MainLayout,
+        children: [
+            {
+                name: "home",
+                path: "/",
+                component: () => import("../views/Home.vue"),
+            },
+            {
+                name: "recipies",
+                path: "/recipies",
+                component: () => import("../views/Recipies.vue"),
+            },
+        ]
     },
 ];
 
---
+++ calories-calculator/src/views/Recipies.vue
@@ -0,0 +1,3 @@
+<template>
+    All recipies
+</template>
