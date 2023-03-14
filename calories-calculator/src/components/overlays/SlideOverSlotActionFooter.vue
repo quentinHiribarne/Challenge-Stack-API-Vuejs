804a01af38384fdcb597d2936ffa82c5195c0f11
Louise BERTIN <louise@trustfolio.co>
2023-03-14 11:05:19 +0100

    Create edit/create form

--- calories-calculator/src/components/overlays/SlideOverSlotActionFooter.vue
+++ calories-calculator/src/components/overlays/SlideOverSlotActionFooter.vue
@@ -7,7 +7,7 @@
                 <div class="absolute inset-0 overflow-hidden">
                     <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
                         <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
-                            <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                            <DialogPanel class="pointer-events-auto w-screen max-w-sm">
                                 <div class="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
                                     <div class="flex min-h-0 flex-1 flex-col overflow-y-scroll py-6">
                                         <div class="px-4 sm:px-6">
--- calories-calculator/src/views/Recipes.vue
+++ calories-calculator/src/views/Recipes.vue
@@ -30,6 +30,9 @@
                                     Auteur
                                 </th>
                                 <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">
+                                    Calories
+                                </th>
+                                <th scope="col" class="py-3.5 px-3 text-left text-sm font-semibold text-gray-900">
                                     Publication
                                 </th>
                                 <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
@@ -39,12 +42,15 @@
                         </thead>
 
                         <tbody class="divide-y divide-gray-200">
-                            <tr v-for="recipe in recipes" :key="recipe.email">
+                            <tr v-for="recipe in recipes" :key="recipe.id">
                                 <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
                                     {{ recipe.title }}
                                 </td>
                                 <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
                                     {{ recipe.author }}
+                                </td>
+                                <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
+                                    {{ recipeCalories(recipe) }}
                                 </td>
                                 <td class="whitespace-nowrap py-4 px-3 text-sm text-gray-500">
                                     {{ recipe.publication_date }}
@@ -69,11 +75,74 @@
 
     <SlideOver
        :open="openSlideOver"
-       :title="recipeToEdit.id ? 'Modifier la recette' : 'Créer une recette'"
+       :title="editing ? 'Modifier la recette' : 'Créer une recette'"
        @close="closeSlideOver" >
     
         <template #content>
-            {{ recipeToEdit }}
+            <div class="flex flex-col gap-4">
+                <div>
+                    <label for="title" class="block text-sm font-medium leading-6 text-gray-900">
+                        Titre
+                    </label>
+                    <div class="mt-2">
+                        <input type="title" name="title" id="title"
+                            v-model="recipe.title"
+                            class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 focus:outline-none sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+                <div>
+                    <label for="author" class="block text-sm font-medium leading-6 text-gray-900">
+                        Auteur
+                    </label>
+                    <div class="mt-2">
+                        <input type="author" name="author" id="author" 
+                            v-model="recipe.author"
+                            class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 focus:outline-none sm:text-sm sm:leading-6" />
+                    </div>
+                </div>
+                
+                <Listbox as="div" v-model="recipe.ingredients" multiple>
+                    <ListboxLabel class="block text-sm font-medium leading-6 text-gray-900">
+                        Ingrédients
+                    </ListboxLabel>
+                    <div class="relative mt-2">
+                        <ListboxButton class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-emerald-500 sm:text-sm sm:leading-6">
+                            <span v-if="recipe.ingredients.length > 0" class="block truncate">
+                                {{ recipe.ingredients.map((ingredient) => ingredient.name).join(', ') }}
+                            </span>
+                            <span class="block truncate text-gray-500">
+                                Sélectionnez un ou plusieurs ingrédients
+                            </span>
+                            <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </span>
+                        </ListboxButton>
+
+                        <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+                            <ListboxOptions class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                <ListboxOption as="template" v-if="ingredients.length > 0"
+                                    v-for="ingredient in ingredients" :key="ingredient.name"
+                                    :value="ingredient"
+                                    v-slot="{ active, selected }">
+                                    <li :class="[active ? 'bg-emerald-500 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
+                                        <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">
+                                            {{ ingredient.name }}
+                                        </span>
+                                        <span v-if="selected" :class="[active ? 'text-white' : 'text-emerald-500', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                                            <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                                        </span>
+                                    </li>
+                                </ListboxOption>
+                                <div v-else>
+                                    <span class="py-2 pl-3 pr-9 font-normal tet-gray-700">
+                                        Aucun ingrédient
+                                    </span>
+                                </div>
+                            </ListboxOptions>
+                        </transition>
+                    </div>
+                </Listbox>
+            </div>
         </template>
     </SlideOver>
 </template>
@@ -81,7 +150,8 @@
 <script setup>
     import { ref, computed } from 'vue';
 
-    import { PencilSquareIcon, TrashIcon } from '@heroicons/vue/24/solid';
+    import { PencilSquareIcon, TrashIcon, CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/24/solid';
+    import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue';
 
     import SlideOver from '../components/overlays/SlideOverSlotActionFooter.vue';
 
@@ -103,22 +173,40 @@
             steps: []
         }
     ]);
+    const ingredients = ref([]);
 
     const openSlideOver = ref(false);
-    const recipeToEdit = ref({});
-    const newRecipe = ref({});
+    const editing = ref(false);
+    const recipe = ref({
+        title: '',
+        author: '',
+        ingredients: [],
+        steps: []
+    });
+
+    const recipeCalories = (recipe) => {
+        let calories = 0;
+        recipe.ingredients.forEach(ingredient => calories = calories + ingredient.calories);
+        return calories;
+    }
 
     const createRecipe = () => {
         openSlideOver.value = true;
     };
-    const editRecipe = (recipe) => {
+    const editRecipe = (editRecipe) => {
         openSlideOver.value = true;
-        recipeToEdit.value = recipe;
+        editing.value = true;
+        recipe.value = editRecipe;
     };
     const deleteRecipe = (recipe) => console.log('delete ' + recipe.id);
 
     const closeSlideOver = () => {
         openSlideOver.value = false;
-        recipeToEdit.value = {};
-    }
+        recipe.value = {
+            title: '',
+            author: '',
+            ingredients: [],
+            steps: []
+        };
+    };
 </script>
