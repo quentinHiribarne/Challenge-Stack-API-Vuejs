18feb5dc4c51badaac7c64ca74bc0e0074449a55
Louise BERTIN <louise@trustfolio.co>
2023-03-15 11:01:22 +0100

    Login logic done

--- calories-calculator/src/views/Login.vue
+++ calories-calculator/src/views/Login.vue
@@ -2,13 +2,15 @@
     <div class="flex min-h-screen">
         <div class="flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
             <div class="mx-auto w-full max-w-sm lg:w-96">
-                <div>
-                    <img class="h-12 w-auto"
-                        src="../assets/logo.jpg" 
-                        alt="Logo de l'application, ensemble d'icônes de nourriture dans un cercle."
-                    />
+                <div class="flex flex-col justify-center text-center h-fit">
+                    <div class="w-fit mx-auto">
+                        <img class="h-36 w-auto"
+                            src="../assets/logo.jpg" 
+                            alt="Logo de l'application, ensemble d'icônes de nourriture dans un cercle."
+                        />
+                    </div>
                     <h2 class="mt-6 text-3xl font-bold tracking-tight text-gray-900">
-                        Connectez-vous à l'application
+                        Accédez à l'application
                     </h2>
                     </div>
 
@@ -16,9 +18,10 @@
 
                 <div class="mt-6">
                     <div>
-                        <button type="submit" 
+                        <button @click="generateToken"
+                            type="button"
                             class="flex w-full justify-center rounded-md bg-emerald-500 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500">
-                            Connection
+                            Entrer
                         </button>
                     </div>
                 </div>
@@ -33,3 +36,14 @@
         </div>
     </div>
 </template>
+
+<script setup>
+    import { useRouter } from "vue-router";
+
+    const router = useRouter();
+
+    const generateToken = () => {
+        localStorage.token = 'fauxTokenCeciEstUnTest';
+        router.push({ name: 'home' });
+    };
+</script>
